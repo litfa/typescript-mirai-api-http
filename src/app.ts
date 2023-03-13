78320af877deed5aa605a61fe5e0f8648474bcb0
ltfei <ltfei@ltfei.cn>
2023-03-13 17:00:32 +0800

    feat: get api

--- src/app.ts
+++ src/app.ts
@@ -149,6 +149,79 @@ const CreateMiraiApi = (
     })
   }
 
+  const getApi = <T>(command: string, content?: object): Promise<T> => {
+    return new Promise((resolve) => {
+      const id = randomId()
+      send({
+        syncId: id,
+        command,
+        content
+      })
+      onMessage<any>((data) => {
+        if (data.syncId == id) {
+          resolve(data)
+        }
+      })
+    })
+  }
+
+  /**
+   * 获取好友列表
+   */
+  const getFriendList = () => getApi<ApiMessage<FriendList>>('friendList')
+  /**
+   * 获取群列表
+   */
+  const getGroupList = () => getApi<ApiMessage<GroupList>>('groupList')
+  /**
+   * 获取群成员列表
+   */
+  const getMemberList = (target: number) => {
+    return getApi<ApiMessage<MemberList>>('memberList', {
+      target
+    })
+  }
+  /**
+   * 获取最新群成员列表
+   */
+  const getLatestMemberList = (target: number, memberIds: number[] = []) => {
+    return getApi<ApiMessage<LatestMemberList>>('latestMemberList', {
+      target,
+      memberIds
+    })
+  }
+  /**
+   * 获取Bot资料
+   */
+  const getBotProfile = () => {
+    return getApi<ApiMessage<BotProfile>>('botProfile')
+  }
+  /**
+   * 获取好友资料
+   */
+  const getFriendProfile = (target: number) => {
+    return getApi<ApiMessage<FriendProfile>>('friendProfile', {
+      target
+    })
+  }
+  /**
+   * 获取群成员资料
+   */
+  const getMemberProfile = (target: number, memberId: number) => {
+    return getApi<ApiMessage<MemberProfile>>('memberProfile', {
+      target,
+      memberId
+    })
+  }
+  /**
+   * 获取QQ用户资料
+   */
+  const getUserProfile = (target: number) => {
+    return getApi<ApiMessage<UserProfile>>('userProfile', {
+      target
+    })
+  }
+
   return {
     ws,
     send,
@@ -160,9 +233,17 @@ const CreateMiraiApi = (
     open,
     onMessage,
     onGroupMessage,
-    onFriendMessage
+    onFriendMessage,
+    getFriendList,
+    getGroupList,
+    getMemberList,
+    getLatestMemberList,
+    getBotProfile,
+    getFriendProfile,
+    getMemberProfile,
+    getUserProfile
   }
 }
 
 export { CreateMiraiApi }
-export type { MessageChainType, MessageType, CommonType }
+export type { MessageChainType, MessageType, CommonType, ApiDataType }
