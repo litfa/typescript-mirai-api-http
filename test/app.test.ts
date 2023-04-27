9d4af2cae885e2c3a29e400f53590a4d7d4844fe
ltfei <ltfei@ltfei.cn>
2023-04-27 10:55:08 +0800

    test: send

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,13 +1,29 @@
 import { IncomingMessage } from 'http'
-import { WebSocket } from 'ws'
+import { WebSocket, RawData } from 'ws'
 import { CreateMiraiApi } from '../src/app'
 import { on } from './eventBus'
-import './ws'
 
+const { send } = CreateMiraiApi('localhost', 3333, 'admin123', 123456, false)
 test('连接参数', () => {
-  CreateMiraiApi('localhost', 3333, 'admin123', 123456)
   on('connection', (ws: WebSocket, req: IncomingMessage) => {
     req.url
     expect(req.url).toBe('/all?verifyKey=admin123&qq=123456')
+  })
+})
+
+describe('api', () => {
+  test('send', () => {
+    on('open', () => {
+      send({
+        type: 'testMessage'
+      })
+    })
+    on('message', (message: RawData) => {
+      expect(message).toBe(
+        JSON.stringify({
+          type: 'testMessage'
+        })
+      )
+    })
   })
 })
